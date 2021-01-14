01ad94985012c083fcfe7a6756e2174686781db2
Black Code Formatter <black_code_formatter@gmail.com>
2021-01-14 00:12:09 +0000

    Black Automatically Formatted Code

--- usage.py
+++ usage.py
@@ -11,71 +11,98 @@ def get_tokens():
     """
     Setting up Tokens you want to expose to the user.
     """
+
     class StatusToken(Token):
         """
         The <code>status</code> Token will return the "sg_status_list" value from the the Shotgun Version.
         """
+
         name = "status"
+
         def getValue(self, data):
             return data.get("sg_status_list")
+
     class LastNameToken(Token):
         """
         The <code>lastname</code> Token will return the "lastname" value of the Artist for the Version.
         """
+
         name = "lastname"
+
         def getValue(self, data):
             return data.get("user.HumanUser.lastname", "")
+
     class FirstNameToken(Token):
         """
         The <code>firstname</code> Token will return the "firstname" value of the Artist for the Version.
         """
+
         name = "firstname"
+
         def getValue(self, data):
             return data.get("user.HumanUser.firstname", "")
+
     class UserToken(Token):
         """
         The <code>user</code> Token will return the "name" value of the Artist for the Version.
         """
+
         name = "user"
+
         def getValue(self, data):
             return data.get("user.HumanUser.name", "")
+
     class ShotToken(Token):
         """
         The <code>shot</code> Token will return the "code" value of the Shot linked to the Version.
         """
+
         name = "shot"
+
         def getValue(self, data):
             return data.get("entity.Shot.code", "")
+
     class SequenceToken(Token):
         """
         The <code>shot</code> Token will return the "code" value of the Sequence linked to the Version.
         """
+
         name = "sequence"
+
         def getValue(self, data):
             value = data.get("entity.Sequence.code", "")
             if value:
                 return value
             return data.get("entity.Shot.sg_sequence.Sequence.code", "")
+
     class ProjectToken(Token):
         """
         The <code>project</code> Token will return the "tank_name" value or the "code" value of the Project linked to the Version.
         """
+
         name = "project"
+
         def getValue(self, data):
             value = data.get("project.Project.tank_name", "")
             return value or data.get("project.Project.code", "") or ""
+
     class NameToken(Token):
         """
         The <code>name</code> Token will return the "code" value of the Version.
         """
+
         name = "name"
+
         def getValue(self, data):
             return data.get("code", "")
+
     class VersionToken(Token):
         """
         The <code>version</code> Token will return the "version_number" value of the PublishedFile linked to the Version.
         """
+
         name = "version"
+
         def getValue(self, data):
             published_files = data.get("published_files", [])
             version_number = None
@@ -86,11 +113,14 @@ def get_tokens():
             if version_number is None:
                 return version_number
             return str(version_number)
+
     class ExtensionToken(Token):
         """
         The <code>extension</code> Token will return the path extension value of the PublishedFile linked to the Version.
         """
+
         name = "extension"
+
         def getValue(self, data):
             published_files = data.get("published_files", [])
             extension = None
@@ -98,15 +128,18 @@ def get_tokens():
                 if published_file.get("path"):
                     path = published_file.get("path")
                     if path.get("link_type") == "local":
-                        local_path = path.get("local_path") or path.get("local_path_linux")
+                        local_path = path.get("local_path") or path.get(
+                            "local_path_linux"
+                        )
                     elif path.get("link_type") == "web":
-                        local_path = path.get("url")[len("file://"):]
+                        local_path = path.get("url")[len("file://") :]
                     else:
                         return None
                     base, ext = os.path.splitext(local_path)
                     extension = ext.lstrip(".")
                     break
             return extension
+
     return [
         StatusToken,
         FirstNameToken,
@@ -128,22 +161,29 @@ def live_version_data():
     """
     # Setting up input data
     from shotgun_api3 import Shotgun
-    sg = Shotgun("https://mysite.shotgunstudio.com", login="mylogin", password="mypassword")
+
+    sg = Shotgun(
+        "https://mysite.shotgunstudio.com", login="mylogin", password="mypassword"
+    )
     fields = [
-        "code", "description",
-        "user.HumanUser.firstname", "user.HumanUser.lastname",
-        "user.HumanUser.name", "sg_status_list", "project.Project.tank_name",
+        "code",
+        "description",
+        "user.HumanUser.firstname",
+        "user.HumanUser.lastname",
+        "user.HumanUser.name",
+        "sg_status_list",
+        "project.Project.tank_name",
         "project.Project.code",
-        "entity.Shot.code", "entity.Shot.sg_sequence.Sequence.code",
+        "entity.Shot.code",
+        "entity.Shot.sg_sequence.Sequence.code",
         "entity.Sequence.code",
         "published_files",
-
     ]
     versions = sg.find(
         entity_type="Version",
         filters=[["project.Project.id", "is", 128]],
         fields=fields,
-        limit=5
+        limit=5,
     )
 
     # Fill out published file data
@@ -151,13 +191,15 @@ def live_version_data():
     for version in versions:
         published_files = version.get("published_files")
         for published_file in published_files:
-            if not published_file.get("version_number") or not published_file.get("path"):
+            if not published_file.get("version_number") or not published_file.get(
+                "path"
+            ):
                 published_files_ids.append(published_file.get("id"))
     if published_files_ids:
         results = sg.find(
             entity_type="PublishedFile",
             filters=[["id", "in", published_files_ids]],
-            fields=["version_number", "path"]
+            fields=["version_number", "path"],
         )
         for result in results:
             for version in versions:
@@ -200,8 +242,12 @@ class Dialog(QtGui.QDialog):
 
         self.template_input_widget = TemplateInputWidget(self.parser)
         self.template_preview_widget = TemplatePreviewWidget(self.version_data)
-        self.template_input_widget.template_changed.connect(self.template_preview_widget.preview_template)
-        self.template_preview_widget.resolve_error.connect(self.template_input_widget.recieve_error)
+        self.template_input_widget.template_changed.connect(
+            self.template_preview_widget.preview_template
+        )
+        self.template_preview_widget.resolve_error.connect(
+            self.template_input_widget.recieve_error
+        )
 
         self.main_page.layout().addWidget(self.template_input_widget)
         self.main_page.layout().addWidget(self.template_preview_widget)
