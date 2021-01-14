952b09294d39a37d79baeb0efdbe2c6c95750f17
Black Code Formatter <black_code_formatter@gmail.com>
2021-01-14 00:18:58 +0000

    Black Automatically Formatted Code

--- sept_qt/__init__.py
+++ sept_qt/__init__.py
@@ -3,5 +3,6 @@ from .input_widget import TemplateInputWidget
 from .preview_widget import TemplatePreviewWidget
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
